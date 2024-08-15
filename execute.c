d0c003e5335e23ebac65608c488afa66ebce4620
Lance <evanslance100@gmail.com>
2024-08-15 06:03:27 -0700

    modified execute.c

--- execute.c
+++ execute.c
@@ -4,14 +4,25 @@ int execute_command(char *input)
 {
 pid_t pid;
 int status;
+char *args[1024];
+char *token;
+int i = 0;
+
+token = strtok(input, " ");
+while (token != NULL)
+{
+args[i++] = token;
+token = strtok(NULL, " ");
+}
+args[i] = NULL;
 
 pid = fork();
 if (pid == 0)
 /* Child process */
 {
-if (execlp(input, input, NULL) == -1)
+if (execvp(args[0], args) == -1)
 {
-perror(input);
+perror(args[0]);
 exit(EXIT_FAILURE);
 }
 }
