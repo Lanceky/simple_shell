5fb4a5f70710364fe0d5658b733c4c216b9e5a81
Lance <evanslance100@gmail.com>
2024-09-01 02:17:13 -0700

    clenaup git commit -m clenaup

--- pid.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(void)
-{
-	pid_t pid = getpid();
-	printf("Process ID: %u\n", pid);
-	return (0);
-}
--- ppid.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(void)
-{
-	pid_t ppid = getppid();
-	printf("Parent process ID: %u\n", ppid);
-	return (0);
-}
