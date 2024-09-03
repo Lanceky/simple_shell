bb468d4178d85df742fbd4f541fb8dd8283475c3
Lance <evanslance100@gmail.com>
2024-09-02 19:21:11 -0700

    created perfect betty checks passing code

--- betty.c
+++ betty.c
@@ -3,19 +3,19 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints the numbers from 0 to 9, each on a new line.
+ * Description: This program prints the numbers from 0 to 9,
+ * each on a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d\n", i);
-    }
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
 
-    return (0);
+	return (0);
 }
-
