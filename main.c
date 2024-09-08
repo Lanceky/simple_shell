d3aa6129531f6244deb0a8df639211d9cd7e284c
Lance <evanslance100@gmail.com>
2024-09-08 09:51:10 -0700

    create betty compliant prompt gen code

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - Entry point of the program
+*
+* @ac: Argument count
+* @argv: Argument vector (array of strings)
+*
+* Return: Always 0 (Success)
+*/
+int main(int ac, char **argv)
+{
+char *prompt = "(Lash) $ ";
+
+printf("%s", prompt);
+
+return (0);
+}
+
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#endif /* MY HEADER FILE */
+
