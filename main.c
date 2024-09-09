59707b7a4e7f75dd4f089021adcb2e44448ced5b
Lance <evanslance100@gmail.com>
2024-09-09 13:51:17 -0700

    added infinite loop to gen prompt and free mem

--- main.c
+++ main.c
@@ -11,9 +11,19 @@
 int main(int ac, char **argv)
 {
 char *prompt = "(Lash) $ ";
-
+char *lineptr;
+size_t n = 0;
+/*Declaring void variables*/
+(void)ac; (void)argv;
+/*An infinite loop*/
+while(1)
+{
 printf("%s", prompt);
+getline(&lineptr, &n, stdin);
+printf("%s\n", lineptr);
 
+free(lineptr);
+}
 return (0);
 }
 
