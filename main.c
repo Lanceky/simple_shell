2a86ab0e8f60b424e6f63541fbe273177dea8a5d
Lance <evanslance100@gmail.com>
2024-09-10 03:47:30 -0700

    added exit code

--- main.c
+++ main.c
@@ -13,13 +13,17 @@ int main(int ac, char **argv)
 char *prompt = "(Lash) $ ";
 char *lineptr;
 size_t n = 0;
+ssize_t nchars_read;
 /*Declaring void variables*/
 (void)ac; (void)argv;
 /*An infinite loop*/
 while(1)
 {
 printf("%s", prompt);
-getline(&lineptr, &n, stdin);
+nchars_read = getline(&lineptr, &n, stdin);
+if (nchars_read == -1){
+            printf("Exiting shell....\n");
+            return (-1);}
 printf("%s\n", lineptr);
 
 free(lineptr);
