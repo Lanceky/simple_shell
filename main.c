c1baba56031fcc9ee13a813c04c70cd35c273298
Lanceky <evanslance100@gmail.com>
2024-10-16 07:51:08 -0700

    intergrated the getline funciton to get the users input

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 int main(int ac, char **argv)
 {
 	char *prompt = "(Lash)$";
+	char *lineptr; //stores the address of the buffer holding whatever was typed
+	ssize_t n = 0; //stores allocated size in bytes
 
 	printf("%s", prompt);
+	getline(&lineptr, &n, stdin); //the getline function that allows us to get what the user types, stdin is the stream(source keyboard)
+	printf("%s", lineptr);
+
+	free(lineptr);
 	return (0);
 }
