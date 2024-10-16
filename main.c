aef709a1e60d7f38febcf33b3d7c8f972535a554
Lanceky <evanslance100@gmail.com>
2024-10-16 08:41:40 -0700

    n data type size_t not ssize_t

--- main.c
+++ main.c
@@ -4,12 +4,15 @@ int main(int ac, char **argv)
 {
 	char *prompt = "(Lash)$";
 	char *lineptr; //stores the address of the buffer holding whatever was typed
-	ssize_t n = 0; //stores allocated size in bytes
-
+	size_t n = 0; //stores allocated size in bytes
+	
+	while (1)
+	{
 	printf("%s", prompt);
 	getline(&lineptr, &n, stdin); //the getline function that allows us to get what the user types, stdin is the stream(source keyboard)
 	printf("%s", lineptr);
 
 	free(lineptr);
+	}
 	return (0);
 }
