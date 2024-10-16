3878ede9833a7239c5d004fddcdaceb0b5d85919
Lanceky <evanslance100@gmail.com>
2024-10-16 07:28:36 -0700

    prompt lash

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main(int ac, char **argv)
+{
+	char *prompt = "(Lash)$";
+
+	printf("%s", prompt);
+	return (0);
+}
--- main.h
+++ main.h
@@ -6,4 +6,4 @@
 #include <unistd.h> /* for unix standard functions like execve*/
 #include <sys/types.h> /*for stat funciton ot check the file status*/
 #include <sys/wait.h> /*  C provides macros and functions that allow a parent process to wait for and retrieve the termination status of its child processes. */
-
+#endif /* MAIN_H */
