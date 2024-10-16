9e8ff534074b52df14d386906d2dbfa7d32f4ffb
Lanceky <evanslance100@gmail.com>
2024-10-16 07:09:38 -0700

    added necessary librariess to header file

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h> /*for input and output operations like printf*/
+#include <stdlib.h> /* for general utilities like malloc and free*/
+#include <string.h> /* for string maninputlation lkke strcpy*/
+#include <unistd.h> /* for unix standard functions like execve*/
+#include <sys/types.h> /*for stat funciton ot check the file status*/
+#include <sys/wait.h> /*  C provides macros and functions that allow a parent process to wait for and retrieve the termination status of its child processes. */
+
