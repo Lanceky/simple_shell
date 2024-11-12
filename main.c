b2168fffc1608d7dbed9c96462ffaeba6bb0d405
Lanceky <evanslance100@gmail.com>
2024-11-12 05:55:05 -0800

    used getline fn return value to exit shell

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(int ac, char **argv)
 char *prompt = "(Lash)$";
 char *lineptr;
 size_t n = 0;
+ssize_t n_read;
 
 /*declaring void variables*/
 (void)ac; (void)argv;
@@ -13,7 +14,12 @@ size_t n = 0;
 while(1)
 {
 printf("%s", prompt);
-getline(&lineptr, &n, stdin);
+n_read = getline(&lineptr, &n, stdin);
+	if(n_read == -1)
+	{
+		printf("Exiting lash.....\n");
+		return(-1);
+	}
 printf("%s\n", lineptr);
 
 free(lineptr);
