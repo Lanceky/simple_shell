a55a713bf2ff8f4f0f09b3fc5506cde0634f0267
Lanceky <evanslance100@gmail.com>
2024-11-12 08:22:23 -0800

    used strcpy to copy the value of lienptr to a new linetr_copy because of strtok fn

--- main.c
+++ main.c
@@ -6,8 +6,9 @@ char *prompt = "(Lash)$";
 char *lineptr;
 size_t n = 0;
 ssize_t n_read;
-
-/*declaring void variables*/
+char *lineptr_copy = NULL;
+const char *delim =  "\n";
+/*declaring void variablesi*/
 (void)ac; (void)argv;
 
 /*infinite loop for prompt*/
@@ -17,11 +18,17 @@ printf("%s", prompt);
 n_read = getline(&lineptr, &n, stdin);
 	if(n_read == -1)
 	{
-		printf("Exiting lash.....\n");
+		printf("Exiting lash....\n");
 		return(-1);
 	}
 printf("%s\n", lineptr);
-
+/*allocate space to lineptr and copy lineptr to lineptr_copy*/
+lineptr_copy = malloc(sizeof(char) * n_read);
+if(lineptr_copy == NULL){
+	perror("tsh: error allcoating memory");
+	return(-1);
+}
+strcpy(lineptr_copy, lineptr);
 free(lineptr);
 }
 return (0);
